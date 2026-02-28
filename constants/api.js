c23218f8b1e552315e6c785d8b5cf3adeae40c3f
shambu <rajeshsabareenadh@gmail.com>
2026-02-28 13:59:33 +0530

    Change backend domain to freepe.in

--- constants/api.js
+++ constants/api.js
@@ -4,6 +4,6 @@ import Constants from "expo-constants";
 // Defaults to Localhost / Custom IP if `.env` is absent during development,
 // but correctly uses EXPO_PUBLIC_API_URL in production / EAS builds.
 
-const API_BASE_URL = process.env.EXPO_PUBLIC_API_URL || "https://indiaapay.com";
+const API_BASE_URL = process.env.EXPO_PUBLIC_API_URL || "https://freepe.in";
 
 export default API_BASE_URL;
